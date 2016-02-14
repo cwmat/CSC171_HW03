77d77ddcc3d96490442728f3f24c188d690b5765
cwmat <chaz.mateer@gmail.com>
2016-02-14 17:23:20 -0500

    feat: Working.  Still want to make a few updates.

--- js/main.js
+++ js/main.js
@@ -12,31 +12,39 @@
    *
    *    return: undefined
    */
-   function generate(data) {
+  function generate(data) {
     // Local var
     var barWidth = 50,
-        barSpacer = 10,
-        labelSpacer = 10,
-        barStart = 250;
+      barSpacer = 10,
+      labelSpacer = 10,
+      barStart = 250;
 
     // Make svg
     var svg = d3.select("#content-charts").append("svg")
-     .attr("width", 700)
-     .attr("height", (barWidth + barSpacer) * data.length);
+      .attr("width", 700)
+      .attr("height", (barWidth + barSpacer) * data.length);
 
     // Draw bar chart
     var bar = svg.selectAll("g")
       .data(data)
       .enter()
       .append("g")
-      .attr("transform", function(d, i) { return "translate(0," + (i * (barWidth + barSpacer)) + ")"; });
+      .attr("transform", function(d, i) {
+        return "translate(0," + (i * (barWidth + barSpacer)) + ")";
+      })
+      .on("click", function(d) {
+        updateContent(d);
+      });
 
     // Draw rectangles
     bar.append("rect")
+      .attr("class", "building-rect")
       .attr("x", barStart)
       // .attr("y", function(d, i) {return (i * barWidth)})
       .attr("height", barWidth - barSpacer)
-      .attr("width", function(d) {return d.height_px;});
+      .attr("width", function(d) {
+        return d.height_px;
+      });
 
     // Write text Label
     bar.append("text")
@@ -45,38 +53,74 @@
       .attr("text-anchor", "end")
       .attr("fill", "black")
       .attr("align", "right")
-      .text(function(d) {return d.building});
+      .text(function(d) {
+        return d.building
+      });
 
     // Write text Label
     bar.append("text")
-      .attr("x", function(d) {return (d.height_px - labelSpacer) + barStart})
+      .attr("x", function(d) {
+        return (d.height_px - labelSpacer) + barStart
+      })
       .attr("y", barWidth / 2)
       .attr("fill", "white")
       .attr("text-anchor", "end")
-      .text(function(d) {return d.height_m});
-   }
+      .text(function(d) {
+        return d.height_m
+      });
+  }
 
-   /* Find Max
-    *    Find the maximum number of characters in an array.
-    *
-    *    return: Max number of characters of array elements.
-    */
-    function findMax(input, key) {
-      var maxChar = 0;
-      if (input) {
-        // Cycle through elements in array if it exists
-        input.forEach(function(element) {
-          // If the element's length is greater than the current max, update max
-          if (element[key].length > maxChar) {
-            maxChar = element[key].length;
-          }
-        });
-        // Return the greatest number of characters of the array elements
-        return maxChar;
-      } else {
-        return 0;
-      }
+  /* Find Max
+   *    Find the maximum number of characters in an array.
+   *
+   *    return: Max number of characters of array elements.
+   */
+  function findMax(input, key) {
+    var maxChar = 0;
+    if (input) {
+      // Cycle through elements in array if it exists
+      input.forEach(function(element) {
+        // If the element's length is greater than the current max, update max
+        if (element[key].length > maxChar) {
+          maxChar = element[key].length;
+        }
+      });
+      // Return the greatest number of characters of the array elements
+      return maxChar;
+    } else {
+      return 0;
     }
+  }
+
+  /* Update Content
+   *    Update information in #content-tables (generally called form a click
+   *    event)
+   *
+   *    return: undefined
+   */
+  function updateContent(data) {
+    if (data) {
+    var name = data.building,
+        height = data.height_m + " m (" + parseInt(data.height_ft).toLocaleString("en-US") + " ft)",
+        city = data.city,
+        country = data.country,
+        floors = data.floors,
+        year = data.completed,
+        imgSrc = "img/" + data.image;
+
+    // Update table
+    $('#bld-name').text(name);
+    $('#bld-height').text(height);
+    $('#bld-city').text(city);
+    $('#bld-country').text(country);
+    $('#bld-floors').text(floors);
+    $('#bld-year').text(year);
+    $('#bld-img').attr("src", imgSrc);
+
+    } else {
+      return undefined;
+    }
+  }
 
   /* Load data
    *    Load the external data and generate the d3 chart.
@@ -99,6 +143,7 @@
       var sorted = data.sort(function(a, b) {
         return b.height_m - a.height_m;
       });
+      updateContent(sorted[0]);
       generate(sorted);
     }
   });
